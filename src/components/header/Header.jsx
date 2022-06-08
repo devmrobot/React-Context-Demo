4e9e0033eea84180aeca7da5165c877507928c69
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2022-06-08 18:15:35 +0200

    adding background to header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import NavBar from '../navbar/NavBar';
-
+import background from "../../assets/tent.jpg";
 import './Header.scss';
 import logo from '../../assets/camping.png';
 import CartCard from '../cartcard/CartCard';
 const Header = () => {
   return (
       <>
-    <div className="Header">
+    <div
+    className="Header"
+    style={{ backgroundImage: `url(${background})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+          }}
+    >
         <div className="LogoContainer">
             <img src={logo} alt="logo" />
             <p className='LogoTitle'>Au campeur fou !</p>

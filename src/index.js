024c60e3850af585ad724d04abcd487aa92456cb
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2022-06-08 13:05:58 +0200

    correcting bug in index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {Router} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
+  <BrowserRouter>
     <App />
-  </Router>
+  </BrowserRouter>
 );
 

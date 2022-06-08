fb2372d0d123da81ea918d66129fc09068f850a3
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2022-06-08 15:38:15 +0200

    cart OK wihtout CSS

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -3,18 +3,41 @@ import { useCart } from '../../contexts/CartProvider';
 
 const Cart = () => {
   const {cart}=useCart();
+
+  let TotalPrice=0;
+
+  cart.forEach((item)=>{
+    let middlePrice=item.qty*item.price;
+    TotalPrice+=middlePrice;
+  });
+
   return (
     <div className="Cart">
       <h1>Votre panier</h1>
       <div className="cartContent">
+        {cart.length!==0&&
+        <div className="CartTable">
+        <table>
+          <tr>
+            <th>Désignation</th>
+            <th>Prix unitaire</th>
+            <th>Quantité</th>
+            <th>Prix total</th>
+          </tr>
         {cart.map((item,index)=>{
           return(
-            <p
-              key={index}>
-                {item.name} - {item.price} - {item.qty}
-              </p>
+            <tr>
+              <td>{item.name}</td>
+                <td>{item.price} €</td> 
+                <td>{item.qty}</td>
+                <td>{item.price*item.qty} €</td> 
+            </tr>  
           )
         })}
+        </table>
+        <p>Prix total : {TotalPrice} €</p>
+        </div>}
+        {cart.length==0&&<p>Votre panier est vide</p>}
       </div>
     </div>
   )

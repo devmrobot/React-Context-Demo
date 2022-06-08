e11cb02e12b22b10fd2a6b57dc492bbf89a608e7
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2022-06-08 10:45:26 +0200

    completing context

--- src/contexts/CartProvider.jsx
+++ src/contexts/CartProvider.jsx
@@ -2,7 +2,12 @@ import {useState, useEffect,createContext,useContext} from 'react';
 const CartContext = createContext(null);
 
 const CartProvider=({children})=>{
-
+    const [cart,setCart]=useState([]);
+    return (
+        <CartContext.provider value={{cart,setCart}}>
+            {children}
+        </CartContext.provider>
+    )
 }
 export const useCart=()=>useContext(CartContext);
 export default CartProvider;
